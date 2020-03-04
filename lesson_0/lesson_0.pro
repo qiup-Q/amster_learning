cfa48af72322898b9e0fba95a542da02b54883df
qiup <qiup@phoneyou.net>
2020-03-04 17:55:46 +0800

    【improve】打印消息；访问BBTick的接口

--- lesson_0/lesson_0.pro
+++ lesson_0/lesson_0.pro
@@ -10,15 +10,22 @@ CONFIG -= debug_and_release build_all
 
 AMSTER_SDK_PATH = $$(AMSTERFWK)
 
-AMSTER_LIB_DEPENDS += notification gbbinfra
+AMSTER_LIB_DEPENDS += notification gbbinfra amsterbb
 include($$AMSTER_SDK_PATH/amster.pri)
 
 DEFINES += DATA_PATH=\\\"$$PWD\\\"
+
+INCLUDEPATH += $$AMSTER_SDK_PATH/sdk/protos/generate_files/cpp
 
 #message(include_paths: $$INCLUDEPATH)
 #message(libs: $$LIBS)
 #message(amster path: $$AMSTER_SDK_PATH)
 
+
 DISTFILES += \
     gbb.json \
     notification.json
+
+PARTY_LIBS += protobuf
+
+include($$(PPM_HOME)/ppm.pri)
--- lesson_0/main.cpp
+++ lesson_0/main.cpp
@@ -5,11 +5,14 @@
 #include <notification/notificationcenter.h>
 #include <gbbinfra/gbbstoragemanager.h>
 
-#include <amster/bb/Message.pb.h>
+#include <google/protobuf/message.h>
+#include <amster/bb/Messages.pb.h>
 
 using namespace amster::notification;
 using namespace amster::gbbinfra;
+using namespace amster::bb;
 using namespace std::chrono_literals;
+namespace pb = google::protobuf;
 
 int main()
 {
@@ -22,9 +25,16 @@ int main()
     std::this_thread::sleep_for(10ms);
 
     auto _listener = _center.registerMessageMonitor("amster.bb.message.*");
-    auto _msg = _center.waitForMonitoredMessage(_listener);
-    BBTick _tick = _msg;
-    _tick.DebugString();
-    std::cout << "listener : " << _listener << std::endl;
+    for(;;){
+        auto _msg = _center.waitForMonitoredMessage(_listener);
+        // BBTick
+        auto _tick = std::dynamic_pointer_cast<const message::BBTick>(_msg);
+        if(_tick){
+            std::cout << "bb time : " << _tick->bb_time_msec()<< std::endl;
+        }else{
+            std::cout << "recv msg : " << _msg->GetTypeName() << " \r\n"
+                      << _msg->DebugString() << std::endl;
+        }
+    }
     return 0;
 }
